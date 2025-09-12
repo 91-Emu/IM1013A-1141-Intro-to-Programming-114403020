3fef3ed696c1713a9b30d9dc6b65764812b878ca
91-Emu <ienjoey7@gmail.com>
2025-09-12 03:57:26 +0000

    temp2

--- week2/temperature_convertion.c
+++ week2/temperature_convertion.c
@@ -2,8 +2,9 @@
 int main(void){
     float ftempF;//temperature in fahrenheit
     float ftempC;//temperature in celsius
-    printf("Enter temperature in fahrenheit\n");//prompt user to enter temperature in fahrenheit
-    scanf("%f,&ftempf");//read temperature in fahrenheit from user
-    ftempC = (5/9) * (ftempF-32);//convert fahrenheit to celsius
-    printf("temperature in celsius is %f\n", ftempC);//print temperature in celsius
+    printf("Enter temperature in celsius\n");//prompt user to enter temperature in celsius
+    scanf("%f",&ftempC);//read temperature in celsius from user
+    ftempF = (9.0/5.0)*ftempC + 32.0;//convert celsius to fahrenheit
+    printf("Temperature in fahrenheit is %.2f\n",ftempF);//print temperature in fahrenheit
+
 }
