9313b5c1493b01a181876ca8dd05fd4dc95b00fa
91-Emu <ienjoey7@gmail.com>
2025-09-26 03:31:43 +0000

    week 3

--- week3/class_average.c
+++ week3/class_average.c
@@ -4,28 +4,44 @@ int main(void){
     int studentcount=0;
     int score=-1;
     float average=0;
+    int pass=0;
+    int fail=0;
+
+    int scoremin=0;
+    int scoremax=100;
 
     puts("Plese enter the score of student 1 or -1 to end ");
+    printf("(%d~%d):",scoremin,scoremax);
     scanf("%d",&score);
 
     while(score!=-1){
-        if(score<0)
+        while((score<scoremin||score>scoremax)&&score!=-1){
             puts("Invalid score, Please enter again");
-        else if(score>100)
-            puts("Invalid score, Please enter again");
-        else{
+            printf("(%d~%d):",scoremin,scoremax);
+            scanf("%d",&score);
+        }//end while
+        if(score!=-1){
             studentcount++;
             total+=score;
             printf("%s%d%s\n","Plese enter the score of student ",studentcount+1," or -1 to end ");
             scanf("%d",&score);
-        }
+            if(score>=60)
+                pass++;
+            else if(score>=0)
+                fail++;
+        }//end if
+        
     }//end while
 
     average=(float)total/(studentcount);
+
     if (studentcount==0)
         puts("No data");
     else
-        printf("total score of %d students is %d,\naverage score is %.2f\n",studentcount,total,average);
+        printf("total score of %d students is %d,\n",studentcount,total);
+        printf("class average is %.2f\n",average);
+        printf("number of pass is %d\n",pass);
+        printf("number of fail is %d\n",fail);
 }//end main        
 
     
