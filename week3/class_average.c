cff8a609e0d45500a55805e3778ad25125e99bd7
91-Emu <ienjoey7@gmail.com>
2025-09-26 02:01:47 +0000

    week 3

--- week3/class_average.c
+++ week3/class_average.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(void){
     int total=0;
-    int studentcount=1;
+    int studentcount=0;
     int score=-1;
     float average=0;
 
@@ -10,22 +10,22 @@ int main(void){
 
     while(score!=-1){
         if(score<0)
-            puts("Invalid score, Please enter a valid score");
+            puts("Invalid score, Please enter again");
         else if(score>100)
-            puts("Invalid score, Please enter a valid score");
+            puts("Invalid score, Please enter again");
         else{
-            total+=score;
             studentcount++;
-            printf("%s%d%s\n","Plese enter the score of student ",studentcount," or -1 to end ");
+            total+=score;
+            printf("%s%d%s\n","Plese enter the score of student ",studentcount+1," or -1 to end ");
             scanf("%d",&score);
         }
     }//end while
 
-    average=(float)total/(studentcount-1);
-    if (studentcount==1)
+    average=(float)total/(studentcount);
+    if (studentcount==0)
         puts("No data");
     else
-        printf("total score of %d students is %d,\naverage score is %.2f\n",studentcount-1,total,average);
+        printf("total score of %d students is %d,\naverage score is %.2f\n",studentcount,total,average);
 }//end main        
 
     
