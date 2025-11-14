7ba100e8d8ece0a5cd6f0eaf71f43b519dd89028
91-Emu <ienjoey7@gmail.com>
2025-11-14 03:51:19 +0000

    w8

--- week8/digit_sum.c
+++ week8/digit_sum.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int iSumlast=0;
+int iSumlast=0;//record last sum
 
 int digit_sum(int);//1234 -> 1+2+3+4=10
 
@@ -8,17 +8,20 @@ int main(){
     puts("Please enter a positive integer");
     unsigned iN=0;
     scanf("%d", &iN);
-
     printf("sum is %d.\n", digit_sum(iN));
-}
+
+}//end main
 
 int digit_sum(int _iN){
-    if(_iN == 0){
+
+    if(_iN == 0){//end recursive
         return 0;
-    }
+    }//end if
+
     int iSum=0;
     iSumlast+=_iN%10;
     printf("%d %3d %6d\n",_iN%10,iSumlast,_iN/10);
     iSum=_iN%10 + digit_sum(_iN/10);
     return iSum;
-}
+
+}//end digit_sum
